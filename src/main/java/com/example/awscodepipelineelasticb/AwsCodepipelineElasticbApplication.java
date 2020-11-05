fcbff186ac05daeb7fae1bf079b9873a0630f2a1
Sol Cordova <sol_cordova@hotmail.com>
2020-11-05 15:34:11 -0600

    Codepipeline AWS tomcat

---
+++ src/main/java/com/example/awscodepipelineelasticb/AwsCodepipelineElasticbApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscodepipelineelasticb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodepipelineElasticbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodepipelineElasticbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awscodepipelineelasticb/AwsCodepipelineElasticbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscodepipelineelasticb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodepipelineElasticbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
